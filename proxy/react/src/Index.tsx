b8a6154eb43b429c5b6c3426a2afda1ded80c431
terut <terut.dev+github@gmail.com>
2018-03-06 02:00:35 +0900

    Add react with typescript

---
+++ proxy/react/src/Index.tsx
@@ -0,0 +1,34 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
+
+const Home = () => (
+  <div>
+    <p>React Home!!</p>
+  </div>
+)
+const About = () => (
+  <div>
+    <p>React About!!</p>
+  </div>
+)
+
+ReactDOM.render(
+  <Router>
+    <div>
+    <ul>
+      <li><Link to="/">Home</Link></li>
+      <li><Link to="/about">About</Link></li>
+    </ul>
+
+    <hr/>
+
+    <Route exact path="/" component={Home} />
+    <Route path="/about" component={About} />
+    </div>
+  </Router>
+, document.getElementById('app'));
